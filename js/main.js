1cbfb77d50538dedd05fc34b9269885a25838036
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2022-01-30 21:55:06 -0600

    FrontEnd Done

--- js/main.js
+++ js/main.js
@@ -13,3 +13,26 @@ submit.addEventListener("click", (e) => {
 close.addEventListener("click", () => {
     modal.classList.toggle("display-none");
 })
+
+//mobile nav
+
+const openIcon = document.querySelector(".fa-bars");
+const closeIcon = document.querySelector(".fa-times-circle");
+const mobileBG = document.querySelector(".mobile-bg");
+const mobileNav = document.querySelector(".mobile-nav");
+
+openIcon.addEventListener("click" , () => {
+    mobileBG.classList.toggle("display-none");
+    mobileNav.classList.remove("slide-out");
+    mobileNav.classList.toggle("slide-in");
+    openIcon.classList.toggle("display-none");
+});
+
+closeIcon.addEventListener("click" , () => {
+    setTimeout(() => {
+        mobileBG.classList.toggle("display-none");
+        openIcon.classList.toggle("display-none");
+    }, 500);
+    mobileNav.classList.toggle("slide-in");
+    mobileNav.classList.toggle("slide-out");
+});
