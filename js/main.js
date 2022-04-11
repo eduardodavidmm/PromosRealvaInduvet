68d5ab16155079271374239e96a7f952a81b06b7
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2022-04-11 14:56:12 -0600

    SuperMascota Upgrade

--- js/main.js
+++ js/main.js
@@ -59,3 +59,24 @@ function sendEmail(params) {
         console.log("SEND", res.status);
     })
 }
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
